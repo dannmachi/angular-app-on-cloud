a96cf4d27df308067534c6da5e94680b3b894cca
Danilo <danilo.eligio@dxc.com>
2023-01-23 00:40:47 +0800

    deligio/v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app-on-cloud';
+  title = 'angular-app-on-cloud-main';
 }
